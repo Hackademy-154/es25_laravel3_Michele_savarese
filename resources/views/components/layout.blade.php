34b3b3687272fbe360b3b89ccc397b16b7eca472
DragonautMk <michelesavarese1997@gmail.com>
2024-10-17 09:45:10 +0200

    import css e js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Es25-Michele Savarese</title>
+    {{-- importare risorse --}}
+    @vite(['resources/css/app.css', ' resources/js/app.js'])
+n
+
     <!-- CDN BOOTSTRAP -->
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -14,8 +18,8 @@
 
 <body>
 
-{{$slot}}
-{{-- //!variabile riservata --}}
+    {{$slot}}
+    {{-- //!variabile riservata --}}
 
 
     {{-- bootstrap script --}}
